8ee95ec308c52202eb9e1ccce8fc8212ef9e44b5
Subrat chandra Naha <89215102+subrat0018@users.noreply.github.com>
2023-04-04 01:56:23 +0530

    Added Logo

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import Logo from "../assets/Logo.png";
 const Navbar = () => {
   const navigate = useNavigate();
   return (
     <nav class="bg-white px-2 shadow-xl sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div class="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="https://flowbite.com/" class="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="h-6 mr-3 sm:h-9"
-            alt="Flowbite Logo"
-          />
+        <Link to="/" class="flex items-center">
+          <img class="w-8 h-8 mr-2 rounded-lg" src={Logo} alt="logo" />
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            NewsFlow
           </span>
-        </a>
+        </Link>
         <div class="flex md:order-2">
           <button
             onClick={() => {
--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Logo from "../assets/Logo.png";
 const Login = () => {
   return (
     <section class="bg-gray-50 dark:bg-gray-900">
@@ -9,12 +9,8 @@ const Login = () => {
           to="/"
           class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         >
-          <img
-            class="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-            alt="logo"
-          />
-          N2AS
+          <img class="w-8 h-8 mr-2 rounded-lg" src={Logo} alt="logo" />
+          NewsFlow
         </Link>
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
