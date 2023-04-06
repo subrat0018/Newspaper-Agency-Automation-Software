614ed031102a58f5459684cf4b6c1ac6a8b15953
Subrat chandra Naha <89215102+subrat0018@users.noreply.github.com>
2023-04-07 03:02:03 +0530

    Added AddDeliveryMan modal

--- Client/src/components/AddDeliveryManModal.jsx
+++ Client/src/components/AddDeliveryManModal.jsx
@@ -66,54 +66,6 @@ const AddDeliveryManModal = ({ setModal }) => {
                   required=""
                 />
               </div>
-              <div>
-                <label
-                  for="language"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Language
-                </label>
-                <input
-                  type="text"
-                  name="language"
-                  id="language"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="English"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  for="category"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Category
-                </label>
-                <select
-                  id="category"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                >
-                  <option selected="">Select category</option>
-                  <option value="TV">TV/Monitors</option>
-                  <option value="PC">PC</option>
-                  <option value="GA">Gaming/Console</option>
-                  <option value="PH">Phones</option>
-                </select>
-              </div>
-              <div class="sm:col-span-2">
-                <label
-                  for="description"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Description
-                </label>
-                <textarea
-                  id="description"
-                  rows="4"
-                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="Write product description here"
-                ></textarea>
-              </div>
             </div>
             <button
               type="submit"
@@ -131,7 +83,7 @@ const AddDeliveryManModal = ({ setModal }) => {
                   clip-rule="evenodd"
                 ></path>
               </svg>
-              Add new Customer
+              Add new Delivery Man
             </button>
           </form>
         </div>
