9aede61761b63dda3d4f31115281b61023c344a5
Subrat chandra Naha <89215102+subrat0018@users.noreply.github.com>
2023-04-07 02:55:34 +0530

    Added AddCustomer RemoveCustomer AddDeliveryMan modals

--- Client/src/components/Box.jsx
+++ Client/src/components/Box.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Box = ({ service }) => {
+const Box = ({ service, setModal }) => {
   return (
     <div className="w-1/4 mb-8">
-      <button class="w-11/12 h-36 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+      <button
+        onClick={() => {
+          console.log(service.split(" ").join(""));
+          setModal(service.split(" ").join(""));
+        }}
+        data-modal-toggle="defaultModal"
+        class="w-11/12 h-36 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+      >
         <span class=" flex flex-col justify-center w-full h-full px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
           <div>{service}</div>
         </span>
--- Client/src/pages/Services.jsx
+++ Client/src/pages/Services.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import Box from "../components/Box";
 import Navbar from "../components/Navbar";
+import AddCustomerModal from "../components/AddCustomerModal";
+import RemoveCustomerModal from "../components/RemoveCustomerModal";
+import AddDeliveryManModal from "../components/AddDeliveryManModal";
 const Services = ({ email }) => {
   const operationForManager = [
     "Add Customer",
     "Remove Customer",
-    "Add Deliver Man",
+    "Add Delivery Man",
     "Remove Delivery Man",
     "Add Product",
     "Remove Product",
@@ -18,17 +21,33 @@ const Services = ({ email }) => {
   const operationForDeliveryMan = ["Get Delivery List", "Add Customer Request"];
   const activeOperations = operationForManager;
   const [currPage, setCurrPage] = useState("Services");
+  const [activeModal, setActiveModal] = useState("");
+  const Modal = () => {
+    switch (activeModal) {
+      case "AddCustomer":
+        return <AddCustomerModal setModal={setActiveModal} />;
+      case "RemoveCustomer":
+        return <RemoveCustomerModal setModal={setActiveModal} />;
+      case "AddDeliveryMan":
+        return <AddDeliveryManModal setModal={setActiveModal} />;
+      default:
+        break;
+    }
+  };
   return (
     <div>
-      <Navbar currPage={currPage} setCurrPage={setCurrPage} />
-      <div className="w-3/4 m-auto mt-20 bg-white px-2 shadow-xl sm:px-4 py-2.5 dark:bg-gray-900 z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-        <h2 className=" font-bold text-3xl text-center mb-8">Services</h2>
-        <div className=" flex flex-wrap">
-          {activeOperations.map((service) => {
-            return <Box service={service} />;
-          })}
+      <div className={`${activeModal ? "opacity-50 bg-gray-300" : ""}`}>
+        <Navbar currPage={currPage} setCurrPage={setCurrPage} />
+        <div className="w-3/4 m-auto mt-20 bg-white px-2 shadow-xl sm:px-4 py-2.5 dark:bg-gray-900 z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+          <h2 className=" font-bold text-3xl text-center mb-8">Services</h2>
+          <div className=" flex flex-wrap">
+            {activeOperations.map((service) => {
+              return <Box service={service} setModal={setActiveModal} />;
+            })}
+          </div>
         </div>
       </div>
+      <Modal />
     </div>
   );
 };
